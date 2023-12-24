971bcaf30dc9a18d22f51e677f83b3c24c4c5e56
Frogy <thefrogeitan@gmail.com>
2023-12-24 21:32:56 +0200

    aa

--- app/src/main/java/com/example/project11/MainActivity.java
+++ app/src/main/java/com/example/project11/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         tv.setText("EitanHagever");
         tv.setTextColor(Color.GREEN);
         tv.setTextSize(50);
+
+        bt = findViewById(R.id.bt1);
+        bt.setTextColor(Color.BLUE);
+        bt.setText("press Me");
+        bt.setBackgroundColor(Color.RED);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("you pressed me!");
+            }
+        });
     }
 }
